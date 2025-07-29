eee58eef363dd12996618e14b9fae1325d88f3f8
Tanu Rai <tanurai4013@gmail.com>
2025-07-29 15:20:57 +0530

    Time: 28 ms (48.65%), Space: 61.4 MB (77.03%) - LeetHub

--- 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.java
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.java
@@ -1,14 +1,24 @@
 class Solution {
     public int[] smallestSubarrays(int[] nums) {
-        int len = nums.length;
+        int n = nums.length;
         int[] lastSeen = new int[30];
-        int[] res = new int[len];
-        for (int i = 0; i < len; i++) res[i] = 1;
-        for (int i = len - 1; i >= 0; i--) {
+        int[] res = new int[n];
+        Arrays.fill(lastSeen, -1);  // Initialize all to -1
+        
+        for (int i = n - 1; i >= 0; i--) {
+            // Update the latest position for each set bit
             for (int bit = 0; bit < 30; bit++) {
-                if ((nums[i] & (1 << bit)) > 0) lastSeen[bit] = i;
-                res[i] = Math.max(res[i], lastSeen[bit] - i + 1);
+                if ((nums[i] & (1 << bit)) != 0) {
+                    lastSeen[bit] = i;
+                }
             }
+            int maxLen = 1;
+            for (int bit = 0; bit < 30; bit++) {
+                if (lastSeen[bit] != -1) {
+                    maxLen = Math.max(maxLen, lastSeen[bit] - i + 1);
+                }
+            }
+            res[i] = maxLen;
         }
         return res;
     }
